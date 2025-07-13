b15dec151c70991bef500fcc40bd03e833f795ef
Sashank Bhagavatula <sashank.bhag@proton.me>
2025-07-12 20:14:28 -0500

    first commit

---
+++ app/api/clerk/route.js
@@ -0,0 +1,41 @@
+import { Webhook } from "svix";
+import connectDB from "@/config/db";
+import User from "@/models/User";
+
+export async function POST(req) {
+    const wh = new Webhook(process.env.SIGNING_SECRET)
+    const headerPayLoad = await headers()
+    const svixHeaders = {
+        "svix-id": headerPayLoad.get("svix-id"),
+        "svix-signature": headerPayLoad.get("svix-signature")
+    };
+
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const {data, type} = wh.verify(body, svixHeaders)
+
+    const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    }
+
+    await connectDB();
+
+    switch (key) {
+    case 'user.created':
+        await User.create(userData)
+        break;
+    case 'user.updated':
+        await User.findByIdAndUpdate(data.id, userData)
+        break;
+    case 'user.deleted':
+        await User.findByIdAndDelete(data.id)
+        break;
+    default:
+        break;
+    }
+
+    return NextRequest.json({message: "Event Received"})
+}
---
+++ context/AppContext.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { createContext, useContext } from 'react';
+import { useUser } from '@clerk/nextjs';
+
+export const AppContext = createContext();
+
+export const useAppContext = ()=> {
+    return useContext(AppContext)
+}
+
+export const AppContextProvider = ({children})=>{
+    const {user} = useUser()
+
+    const value = {
+        user
+    }
+
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>
+}
