a227470aa46d71c3704e5640755eb1eb197bc20d
Sashank Bhagavatula <sashank.bhag@proton.me>
2025-07-14 19:08:40 -0500

    6th commit

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -1,6 +1,8 @@
 import { Webhook } from "svix";
-import connectDB from "../../../config/db";
-import User from "../../../models/user";
+import connectDB from "@/config/db";
+import User from "@/models/user";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
 export async function POST(req) {
     const wh = new Webhook(process.env.SIGNING_SECRET)
