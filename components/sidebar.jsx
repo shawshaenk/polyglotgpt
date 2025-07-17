c0d31e71b37ddda7ca07f80d60ddc6a798e32a75
Sashank Bhagavatula <sashank.bhag@proton.me>
2025-07-16 22:32:50 -0500

    tweaked sidebar buttons for minimalism

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -45,8 +45,8 @@ const Sidebar = ({ expand, setExpand }) => {
       className={`flex flex-col justify-between bg-[#2a2a2a] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${
         expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
       <div>
-        <div className={`flex ${expand ? "flex-row gap-10" : "flex-col items-center gap-8"}`}>
-          <Image className={expand ? "w-36" : "w-10"} src={expand ? assets.logo_text : assets.logo_icon} alt="Logo" />
+        <div className={`flex ${expand ? "flex-row justify-end px-4" : "flex-col items-center gap-8"}`}>
+          {/* <Image className={expand ? "w-36" : "w-10"} src={expand ? assets.logo_text : assets.logo_icon} alt="Logo" /> */}
 
           <div onClick={()=> expand ? setExpand(false) : setExpand(true)}
           className="group relative flex items-center justify-center hover:bg-gray-500 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer">
@@ -56,21 +56,18 @@ const Sidebar = ({ expand, setExpand }) => {
               alt="Toggle"
               className="hidden md:block w-7"
             />
-            <div className={`absolute w-max ${expand ? "left-1/2 -translate-x-1/2 top-12" : "-top-12 left-0"} opacity-0 group-hover:opacity-100
-            transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
+            <div className={`absolute left-full ml-3 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none ${expand ? 'w-30' : 'w-29'}`}>
                 {expand ? "Close Sidebar" : "Open Sidebar"}
-                <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "left-1/2 -top-1.5 -translate-x-1/2" : "left-4 -bottom-1.5"}`}></div>
+                <div className="w-3 h-3 absolute bg-black rotate-45 left-1.5 -translate-x-full top-1/2 -translate-y-1/2"></div>
             </div>
           </div>
         </div>
 
-        <button onClick={createNewChat} className={`mt-8 flex items-center justify-center cursor-pointer 
-          ${expand ? "bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max" : "group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+        <button onClick={createNewChat} className={`${expand ? "absolute top-5.5 left-6 bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5" : "mt-8 flex justify-center group h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"} flex items-center cursor-pointer`}>
           <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=""/>
-          <div className="absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black 
-          text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none">
+          <div className="absolute left-full ml-3 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none w-23">
             New Chat
-            <div className="w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5"></div>
+            <div className="w-3 h-3 absolute bg-black rotate-45 left-1.5 -translate-x-full top-1/2 -translate-y-1/2"></div>
           </div>
           {expand && <p className="text-white text font-medium">New Chat</p>}
         </button>
